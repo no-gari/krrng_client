68a339a7f1117f35467b3e6283d9372814e060aa
doitduri <kdle_@naver.com>
2022-11-12 23:23:23 +0900

    현재 위치 마커 표시 기능 구현 (마커 아이콘 미적용)

--- lib/modules/hospital/view/hospital_page.dart
+++ lib/modules/hospital/view/hospital_page.dart
@@ -2,35 +2,67 @@ import 'dart:async';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:krrng_client/modules/hospital/cubit/hospital_cubit.dart';
 
 class HospitalPage extends StatefulWidget {
   @override
   _HospitalPageState createState() => _HospitalPageState();
 }
 
-class _HospitalPageState extends State<HospitalPage>
-    with AutomaticKeepAliveClientMixin {
-  @override
-  bool get wantKeepAlive => true;
+class _HospitalPageState extends State<HospitalPage> {
+  List<Marker> _markers = [];
 
+  NaverMapController? _naver;
+  late HospitalCubit _hospitalCubit;
+
+  @override
+  void initState() {
+    super.initState();
+    _hospitalCubit = BlocProvider.of<HospitalCubit>(context);
+    _hospitalCubit.setPosition();
+  }
   @override
   Widget build(BuildContext context) {
-
-    final sampleList =
-        "이 예제는 지도를 리스트뷰 위에서 사용할 때, Gesture를 Map에 우선적으로 전달하기 위한 경우를 위해 제공되는 예제입니다."
-            .split(" ");
-
-    return Scaffold(
-        body: NaverMap(
-      onCameraChange: (l, _, __) {
-        print(l);
-      },
-      initialCameraPosition:
-          CameraPosition(target: LatLng(37.5425, 126.9669), zoom: 17),
-      initLocationTrackingMode: LocationTrackingMode.None,
-      // forceGesture: true,
-    ));
-    ;
+    return BlocBuilder<HospitalCubit, HospitalState>( builder: (context, state) {
+      if (state.isLoaded ?? false) {
+        return NaverMap(
+          markers: [
+            Marker(
+                markerId: DateTime.now().toIso8601String(),
+                position: state.location!,
+                captionText: "현재 위치",
+                captionColor: Colors.indigo,
+                captionTextSize: 20.0,
+                alpha: 0.8,
+                captionOffset: 30,
+                anchor: AnchorPoint(0.5, 1),
+                // icon: OverlayImage.fromAssetImage(assetName: 'assets/icons/marker.png')
+                width: 45,
+                height: 45,
+                infoWindow: state.currentPlace,
+                onMarkerTab: _onMarkerTap)
+          ],
+          initLocationTrackingMode: LocationTrackingMode.Follow,
+          onMapCreated: (controller) {
+            this._naver = controller;
+          },
+        );
+      } else {
+        return Center(child: CircularProgressIndicator());
+      }
+    });
   }
+
+  // ================== method ==========================
+  void _onMarkerTap(Marker? marker, Map<String, int?> iconSize) {
+    int pos = _markers.indexWhere((m) => m.markerId == marker?.markerId);
+    setState(() {
+      this._markers[pos].captionText = '선택됨';
+      var position = this._markers[pos].position;
+      this._naver?.moveCamera(CameraUpdate.scrollTo(position!));
+    });
+  }
+
 }
--- lib/modules/hospital/view/hospital_screen.dart
+++ lib/modules/hospital/view/hospital_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:krrng_client/modules/hospital/cubit/hospital_cubit.dart';
+import 'package:krrng_client/repositories/map_repository/map_repository.dart';
 import 'hospital_page.dart';
 
 class HospitalScreen extends StatelessWidget {
@@ -6,6 +9,11 @@ class HospitalScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return HospitalPage();
+    return Scaffold(
+      body: BlocProvider(
+        create: (context) => HospitalCubit(RepositoryProvider.of<MapRepository>(context)),
+        child: HospitalPage(),
+      ),
+    );
   }
 }
