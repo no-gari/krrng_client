40da6cd5c720e170e43692d50a046db361633f9f
doitduri <kdle_@naver.com>
2022-11-17 20:55:36 +0900

    User model 변경 (firebase 삭제)

--- lib/repositories/user_repository/models/user.dart
+++ lib/repositories/user_repository/models/user.dart
@@ -2,26 +2,23 @@ import 'package:equatable/equatable.dart';
 
 class User extends Equatable {
   const User(
-      {this.nickname, this.profileImage, this.points, this.firebaseToken});
+      {this.nickname, this.profileImage, this.points});
 
   final String? nickname;
   final String? profileImage;
   final int? points;
-  final String? firebaseToken;
 
   @override
-  List<Object?> get props => [nickname, profileImage, points, firebaseToken];
+  List<Object?> get props => [nickname, profileImage, points];
 
-  User copyWith(
-      {String? nickname,
+  User copyWith({String? nickname,
       String? profileImage,
-      int? points,
-      String? firebaseToken}) {
+      int? points
+      }) {
     return User(
       nickname: nickname ?? this.nickname,
       profileImage: profileImage ?? this.profileImage,
       points: points ?? this.points,
-      firebaseToken: firebaseToken ?? this.firebaseToken,
     );
   }
 
