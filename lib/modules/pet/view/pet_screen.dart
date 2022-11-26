ec7c19bf293b438f6b1b03ef1c0212aa5af2dbb7
doitduri <kdle_@naver.com>
2022-11-27 00:09:45 +0900

    pet screen queryparams 로직 추가

--- lib/modules/pet/view/pet_screen.dart
+++ lib/modules/pet/view/pet_screen.dart
@@ -3,14 +3,19 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:krrng_client/modules/pet/cubit/pet_cubit.dart';
 import 'package:krrng_client/modules/pet/view/pet_page.dart';
 import 'package:krrng_client/repositories/animal_repository/animal_repository.dart';
+import 'package:vrouter/vrouter.dart';
 
 class PetScreen extends StatelessWidget {
   static const String routeName = '/pet';
 
   @override
   Widget build(BuildContext context) {
+    final editValue = context.vRouter.queryParameters["edit"];
     return BlocProvider(
-        create: (context) => PetCubit(RepositoryProvider.of<AnimalRepository>(context)),
+        create: (context) => PetCubit(
+            RepositoryProvider.of<AnimalRepository>(context),
+            (editValue ?? "false") == "true" ? true : false
+        ),
         child: PetRegisterPage()
     );
   }
