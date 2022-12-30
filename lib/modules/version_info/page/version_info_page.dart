7b3f0adc70e1bce702cb7fddc8c27e238970c429
no-gari <krrnghj@naver.com>
2022-12-30 11:41:05 +0900

    finish version-info page

--- lib/modules/version_info/page/version_info_page.dart
+++ lib/modules/version_info/page/version_info_page.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:krrng_client/modules/version_info/cubit/version_info_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:package_info/package_info.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/material.dart';
 
 class VersionInfoPage extends StatefulWidget {
   const VersionInfoPage({Key? key}) : super(key: key);
@@ -12,10 +14,23 @@ class VersionInfoPage extends StatefulWidget {
 class _VersionInfoPageState extends State<VersionInfoPage> {
   late VersionInfoCubit _versionInfoCubit;
 
+  PackageInfo _packageInfo = PackageInfo(
+      appName: 'Unknown',
+      packageName: 'Unknown',
+      version: 'Unknown',
+      buildNumber: 'Unknown');
+
   @override
   void initState() {
     super.initState();
+    _initPackageInfo();
     _versionInfoCubit = BlocProvider.of<VersionInfoCubit>(context);
+    _versionInfoCubit.getAppversion();
+  }
+
+  Future<void> _initPackageInfo() async {
+    final PackageInfo info = await PackageInfo.fromPlatform();
+    setState(() => _packageInfo = info);
   }
 
   @override
@@ -24,22 +39,68 @@ class _VersionInfoPageState extends State<VersionInfoPage> {
         appBar: AppBar(
             centerTitle: false,
             title: Text('버전 정보', style: Theme.of(context).textTheme.headline2)),
-        body: SafeArea(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-              SizedBox(height: MediaQuery.of(context).size.height * 0.2),
-              Center(child: Image.asset('assets/images/version_info.png')),
-              SizedBox(height: 60),
-              Center(
-                  child: Text('현재 최신 버전을 사용 중입니다.',
-                      style: TextStyle(
-                          fontSize: 16, fontWeight: FontWeight.bold))),
-              SizedBox(height: 10),
-              Center(
-                  child: Text('현재 버전 1.1',
-                      style:
-                          TextStyle(fontSize: 22, fontWeight: FontWeight.w900)))
-            ])));
+        body: BlocBuilder<VersionInfoCubit, VersionInfoState>(
+            builder: (context, state) {
+          if (state.isLoaded == true && _packageInfo.version != 'Unknown') {
+            return SafeArea(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+                  Center(child: Image.asset('assets/images/version_info.png')),
+                  SizedBox(height: 60),
+                  if (state.version == num.parse(_packageInfo.version))
+                    Column(children: [
+                      Center(
+                          child: Text('현재 최신 버전을 사용 중입니다.',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold))),
+                      SizedBox(height: 10),
+                      Center(
+                          child: Text('현재 버전 ${state.version.toString()}V',
+                              style: TextStyle(
+                                  fontSize: 22, fontWeight: FontWeight.w900)))
+                    ])
+                  else
+                    Column(children: [
+                      Center(
+                          child: Text('현재 버전 ${_packageInfo.version}V',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold))),
+                      SizedBox(height: 10),
+                      Center(
+                          child: Text('최신 버전 ${state.version.toString()}V',
+                              style: TextStyle(
+                                  fontSize: 22, fontWeight: FontWeight.w900))),
+                      SizedBox(height: 10),
+                      InkWell(
+                        onTap: _launchURL,
+                        child: Container(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 18, vertical: 10),
+                            color: Theme.of(context).primaryColor,
+                            child: Text('버전 업데이트 하기',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white)),
+                            alignment: Alignment.center),
+                      )
+                    ])
+                ]));
+          }
+          return Center(
+              child: Image.asset('assets/images/indicator.gif',
+                  width: 100, height: 100));
+        }));
+  }
+}
+
+_launchURL() async {
+  const url = 'https://onelink.to/82ttrz';
+  if (await canLaunchUrl(Uri.parse(url))) {
+    await launchUrl(Uri.parse(url));
+  } else {
+    throw 'Could not launch $url';
   }
 }
